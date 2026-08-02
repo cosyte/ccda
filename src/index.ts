bdd725e943025a689b190f5972b2443d52673a50
Noah Schatz <noah.lane.schatz@gmail.com>
2026-08-02 15:29:41 -0400

    Version Packages (#81)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@
  * console.log(VERSION);
  * ```
  */
-export const VERSION: string = "0.0.5";
+export const VERSION: string = "0.0.6";
 
 // Parser entry point + options.
 export { parseCcda } from "./parser/index.js";
